236b8b6b38a762ae9365ae47d67a9e3d88c30943
Trevor Rothaus <trevor.rothaus@sendgrid.com>
2015-03-01 18:04:59 -0800

    Rearranges the js a little. More border-radius. Added jquery file

--- web/kafka_viz/js/app.js
+++ web/kafka_viz/js/app.js
@@ -1,118 +1,122 @@
 $(document).ready(function(){
-  console.log('Kyafkuh. You have to say it like that. ');
-
-  //begin tech debt
-  createTopic = function(result) {
-    for(i in result.result) {
-
-      if(result.result[i].name === ""){
-        document.write("No Topics Found!");
-      } else {
-        var topicName = result.result[i].name;
-        var replicationNum = result.result[i].replication;
-        var partitionNum = result.result[i].partitions;
-      }
-
-      var partitionClick = function(topicName, partition, partitionLength) {
-        return function() {
-          partitionLength -= 1;
-          var lenMinusFive = partitionLength - 5;
-          var partitionRange = $('#'+topicName+"PartitionRange").val();
-          if (partitionRange === "") {
-            partitionRange = "" + lenMinusFive + "-" + partitionLength;
-            $('#'+topicName+"PartitionRange").val(partitionRange);
-          }
-
-          var url = "/topics/"+topicName+"/"+partition+"/"+partitionRange;
-          $.get(url, function(result) {
-            console.log(result);
-            $('#'+topicName+"Data").html(result.join("<br>"));
-            $('#'+topicName+"Data").show();
-          });
-        };
-      }
-
-      var clickFun = function(topicName) {
-        return function() {
-          // var url = "http://private-e3c89-kafkahttp.apiary-mock.com/topics/"+topicName
-          var url = "/topics/"+topicName
-            data=$("#"+topicName).val();
-          $.post( url,
-              {data:data},
-              function( result ) {
-                console.log(topicName)
-                  $("#"+topicName).val("");
-                  $('main').html("");
-                  loadTopics();
-              })
-          .fail(function() {
-            $('.rightFloat').append("<div class='errorBox'<p>Error POSTing!</p>");
-          })
-        }
-      }
-
-      //Create variables for divs & classes
-      var newContainer = $( "<div class='container'/>" );
-      var newSubTitle = $( "<div class='subTitle' />");
-      var newTopic = $( "<div class='topic' />" );
-      var newLeft = $( "<div class='leftFloat' />");
-      var newRight = $( "<div class='rightFloat' />");
-      var newExport = $( "<div class='dataInput android-input-wrapper'>"+
-                         "<input type='text' id='"+topicName+"'"+
-                         "placeholder='Add Data to Topic' class='android-input dataInput'"+
-                         "name='customerEmail' />"+
-                         "</div>");
-      var newPartitionRange = $( "<input type=text id='"+topicName+"PartitionRange' placeholder='partition range'/>" )
-      var newSubmitBtn = $( "<input class='btn dataSubmit' type='submit' value='Submit'/><br></form>");
-      var newDataArea = $( "<div class='data', id='"+topicName+"Data';></div>");
-      newDataArea.hide();
-
-      var partitionAddButton = $("<div class='partitionButtons'><a class='btn-floating btn-medium waves-effect waves-light lightteal'><i class='mdi-content-add'></i></a> </div>");
-
-      newSubmitBtn.click(clickFun(topicName));
-
-      $('main').append(newContainer);
-      $('main').append(newContainer);
-      newContainer.append(newSubTitle);
-      newContainer.append(newTopic);
-      newTopic.append(newLeft);
-      newTopic.append(newRight);
-      newLeft.append(partitionAddButton);
-      newRight.append(newPartitionRange);
-      newRight.append(newDataArea);
-
-      newSubTitle.append("<h5>"+topicName+"</h5>");
-      newSubTitle.append(newExport);
-      newSubTitle.append(newSubmitBtn);
-      var newAddButton = $( "<div class='partitionButtons' /><a class='btn-floating btn-medium waves-effect waves-light lightteal'><i class='mdi-content-add'></i></a><br><br>");
-      newSubTitle.append("<h6>"+"partition(s): "+partitionNum+",  "+"replication factor: "+replicationNum+"</h6>");
-
-
-      for(j in result.result[i].partition_info){
-        partitionLength = result.result[i].partition_info[j].length;
-        var partitionHTML = $("<div class='btn partition z-depth-1'>"+partitionLength+"</div>")
-        newLeft.append(partitionHTML);
-        partitionHTML.click(partitionClick(topicName, j, partitionLength));
-      }
-    }
-  };
-
-
-  //end tech debt
   loadTopics();
-
-  //To Do list:
-  //On-off KYAFKUHH toggle button
-  //Create topic API
-  //Reload only single topic
-
 })
+
 
 function loadTopics() {
   var url = "/topics"
-  $.ajax({
-    url: url,
-    success: createTopic
+    $.ajax({
+      url: url,
+      success: createTopics
 
-  });
+    });
 }
+
+var publishMessage = function(topicName) {
+  return function() {
+    // var url = "http://private-e3c89-kafkahttp.apiary-mock.com/topics/"+topicName
+    var url = "/topics/"+topicName
+      data=$("#"+topicName).val();
+    $.post( url,
+        {data:data},
+        function( result ) {
+          console.log(topicName)
+            $("#"+topicName).val("");
+          $('main').html("");
+          loadTopics();
+        })
+    .fail(function() {
+      $('.rightFloat').append("<div class='errorBox'<p>Error POSTing!</p>");
+    })
+  }
+}
+
+// Returns partition range from topic input box
+// Returns default newest 5, if not specified
+var partitionRange = function(topicName, partitionLength) {
+
+    var partitionRange = $('#'+topicName+"PartitionRange").val();
+
+    if (partitionRange === "") {
+      partitionLength -= 1;
+      var lenMinusFive = partitionLength < 5 ? 0 : partitionLength - 5;
+      partitionRange = "" + lenMinusFive + "-" + partitionLength;
+      $('#'+topicName+"PartitionRange").val(partitionRange);
+    }
+    return partitionRange;
+}
+
+var partitionClick = function(topicName, partition, partitionLength) {
+  return function() {
+    range = partitionRange(topicName, partitionLength);
+
+    var url = "/topics/"+topicName+"/"+partition+"/"+range;
+    $.get(url, function(result) {
+      $('#'+topicName+"Data").html(result.join("<br>"));
+      $('#'+topicName+"Data").show();
+    });
+  };
+}
+
+
+createTopics = function(result) {
+  for(i in result.result) {
+    topic = result.result[i];
+
+    if(topic.name === ""){
+      document.write("No Topics Found!");
+      return;
+    }
+
+    var topicName = topic.name;
+    var replicationNum = topic.replication;
+    var partitionNum = topic.partitions;
+
+    //Create variables for divs & classes
+    var newContainer = $( "<div class='container'/>" );
+    var newSubTitle = $( "<div class='subTitle' />");
+    var newTopic = $( "<div class='topic' />" );
+    var newLeft = $( "<div class='leftFloat' />");
+    var newRight = $( "<div class='rightFloat' />");
+    var newExport = $( "<div class='dataInput android-input-wrapper'>"+
+        "<input type='text' id='"+topicName+"'"+
+        "placeholder='Add Data to Topic' class='android-input dataInput'"+
+        "name='customerEmail' />"+
+        "</div>");
+    var newPartitionRange = $( "<div class='partitionRange'><input type=text id='"+topicName+"PartitionRange' placeholder='partition range'/></div>" );
+    var newSubmitBtn = $( "<input class='btn dataSubmit' type='submit' value='Submit'/><br></form>");
+    var newDataArea = $( "<div class='data', id='"+topicName+"Data';></div>");
+    var clear = $( "<div class='clear'></div>" );
+    var newTopicNavBar = $( "<div class='subNavBar'></div>");
+    newDataArea.hide();
+
+    var partitionAddButton = $("<div class='partitionButtons'><a class='btn-floating btn-medium waves-effect waves-light lightteal'><i class='mdi-content-add'></i></a> </div>");
+
+    newSubmitBtn.click(publishMessage(topicName));
+
+    $('main').append(newContainer);
+    $('main').append(newContainer);
+    newContainer.append(newSubTitle);
+    newContainer.append(newTopic);
+    newTopic.append(newTopicNavBar);
+    newTopic.append(newLeft);
+    newTopic.append(newRight);
+    newTopicNavBar.append(partitionAddButton);
+    newTopicNavBar.append(newPartitionRange);
+    newRight.append(newDataArea);
+
+    newSubTitle.append("<h5>"+topicName+"</h5>");
+    newSubTitle.append(newExport);
+    newSubTitle.append(newSubmitBtn);
+    var newAddButton = $( "<div class='partitionButtons' /><a class='btn-floating btn-medium waves-effect waves-light lightteal'><i class='mdi-content-add'></i></a><br><br>");
+    newSubTitle.append("<h6>"+"partition(s): "+partitionNum+",  "+"replication factor: "+replicationNum+"</h6>");
+
+
+    for(j in topic.partition_info){
+      partitionLength = topic.partition_info[j].length;
+      var partitionHTML = $("<div class='btn partition z-depth-1'>"+partitionLength+"</div>");
+      newLeft.append(partitionHTML);
+      partitionHTML.click(partitionClick(topicName, j, partitionLength));
+    }
+  }
+};
